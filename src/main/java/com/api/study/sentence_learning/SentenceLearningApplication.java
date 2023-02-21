71dc28bebc90b3172cd90fb4c7351a93a20fc66b
deco2040 <deco2040@naver.com>
2023-02-22 02:47:48 +0900

    1st commit

---
+++ src/main/java/com/api/study/sentence_learning/SentenceLearningApplication.java
@@ -0,0 +1,13 @@
+package com.api.study.sentence_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentenceLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentenceLearningApplication.class, args);
+	}
+
+}
